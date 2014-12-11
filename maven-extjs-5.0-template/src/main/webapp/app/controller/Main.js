aa06626c5e5f5bd6f310dbc51f7601b60a6b4ee8
Bala <balagangadhar85@gmail.com>
2014-12-11 22:27:25 +0530

    Added controller

---
+++ maven-extjs-5.0-template/src/main/webapp/app/controller/Main.js
@@ -0,0 +1,12 @@
+Ext.define('MyApp.controller.Main', {
+	extend : 'Ext.app.Controller',
+	stores : [],
+	models : [],
+	views : [],
+	init : function() {
+		console.log('MyApp.controller.Main - Initialized');
+		this.control({
+
+		});
+	}
+});
